a9ded407b64d2fe5f5ecdff21cc228a9af947ffd
Abhishek kumar <aasalex21c@gmail.com>
2023-11-04 11:55:52 +0530

    #1. inside doublyLinkedList.js added new methods: shift(), unshift() and get() #2. created poster image for this repository using photoshop!

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -11,8 +11,89 @@ class Node{
 class DoublyLinkedList{
   constructor(val){
       this.length=0;           
-      this.push(val);  
+      this.head=null;
+      this.tail=null;  
   }
+/***
+ * Get the node iff present at the index provided
+ */
+  get(idx){
+    // Safeguard
+      if((idx < 0) || (idx > this.length-1)){
+        return false;
+      }
+    let counter, currentNode;
+    // Case #1 index is closer to head node
+      if(idx< Math.floor(this.length/2)){
+        console.log("Near Head");
+        counter=0; currentNode=this.head;
+        while(counter !== idx){
+          ++counter;
+          currentNode = currentNode.next;
+        }
+        return currentNode;
+      }
+      // Case #2 index is closer to tail node
+      else{
+        console.log("Near Tail");
+        counter=this.length-1; currentNode=this.tail;
+        while(counter !== idx){
+          --counter;
+          currentNode = currentNode.previous;
+        }        
+        return currentNode;
+      }
+  }  
+/***
+ * Add node to the beginning of the Doubly Linked List
+ */
+  unshift(val){
+    // Case #1 : List is empty
+      if(this.length===0){
+        let newNode = new Node(val);
+        this.head=newNode;
+        this.tail=newNode;
+        this.length++;
+        return this;     
+      }
+    // Case #2: List is having one or more nodes
+      else if(this.length > 0){
+        let headNode = this.head;
+        let newNode = new Node(val);              
+        newNode.next = headNode;
+        headNode.previous = newNode;
+        this.head = newNode;
+        ++this.length;
+        return this;
+      }
+  }  
+
+/***
+ * Removes the node from the beginning of the doubly linked list
+ */
+  shift(){
+    // Case #1: List is having Nodes
+      // Case #1.1: List is having only single Node
+      if(this.length == 1){
+        this.head=null;
+        this.tail=null;
+        this.length =0;
+        return this;
+      }    
+      // Case #1.2 List is having more than one nodes
+      else if(this.length > 1){
+        let nextNode = this.head.next;
+        nextNode.previous = null;
+        this.head=nextNode;
+        --this.length;
+        return this;
+      }    
+    // Case #2: List is empty
+    else if(this.length === 0){
+      return false;
+    }
+  }        
+  
 /***
  * Remove the element from the end of the doubly linked list
  */
@@ -57,8 +138,6 @@ class DoublyLinkedList{
       }
     // Step #2 Case #2 It does not have any nodes
       else{
-        newNode.next=null;
-        newNode.next=null;
         this.head = newNode;
         this.tail = newNode;
         ++this.length;  
@@ -68,8 +147,18 @@ class DoublyLinkedList{
 
 } 
 
-let myDoublyLinkedList = new DoublyLinkedList(29);
+let myDoublyLinkedList = new DoublyLinkedList();
+myDoublyLinkedList.push(5);
+myDoublyLinkedList.push(10);
+myDoublyLinkedList.push(15);
+myDoublyLinkedList.push(20);
+myDoublyLinkedList.push(25);
 myDoublyLinkedList.push(30);
-myDoublyLinkedList.push(42);
-myDoublyLinkedList.push(52);
+myDoublyLinkedList.push(35);
+myDoublyLinkedList.push(40);
+myDoublyLinkedList.push(55);
+myDoublyLinkedList.push(50);
+myDoublyLinkedList.push(55);
+myDoublyLinkedList.push(60);
+
 console.log(myDoublyLinkedList);
