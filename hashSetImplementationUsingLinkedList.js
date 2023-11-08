ac3193fbbf4c901aa1ddb883dec038adb8e3e4b3
Abhishek kumar <aasalex21c@gmail.com>
2023-11-08 17:25:03 +0530

    hashMap working when size of hashTable i'm setting to 10, but not working when setting it to 20 or more, don't know why, figuring out, still need to work on it

--- hashSetImplementationUsingLinkedList.js
+++ hashSetImplementationUsingLinkedList.js
@@ -51,31 +51,50 @@ class DoublyLinkedList{
             return false;
         }
         let currentNode = this.head;
-        if(this.length===1){
+        if(this.length===1 && this.head.val==key){
             this.head=null;
             this.tail=null;
             this.length=0;
             return true;
         }
-        while(currentNode){
-            if(currentNode.val === key){
-                // breaking the link
-                let previousNode = currentNode?.previous;
-                let nextNode = currentNode?.next;
-                if(previousNode?.previous){
-                    previousNode.next = nextNode;
-                }else{
-                    this.head=nextNode;
+        let idx=0;
+        while(currentNode){          
+            if(currentNode.val === key){          
+              // breaking the link
+              // Case #1: Remove last node
+                if(this.length === idx+1){
+                  // console.log('okay you want to remove last node');
+                  if(this.length > 0){
+                    // console.log('there are more than 1 nodes in the list');
+                    let previousNode = currentNode.previous;
+                    this.tail = previousNode;
+                    --this.length;
+                    return true;
+                  }
                 }
-                if(nextNode?.previous){
-                    nextNode.previous = previousNode;
-                }else{
-                    this.tail=previousNode;
-                }
+              // Case #2: it is middle node  
+              else if(this.length > 1 && idx!==0){
+                // console.log('it is the midle node');
+                let previousNode =currentNode.previous;
+                let nextNode = currentNode.next;
+                previousNode.next = nextNode;
+                nextNode.previous = previousNode;
                 --this.length;
-               return true;
+                return true;
+              }
+              // Case #3: the very first node
+              else if(this.length>=1 && idx+1==1){
+                // console.log('very first node');
+                let nextNode = currentNode.next;
+                this.head = nextNode;
+                this.head.previous=null;
+                nextNode.previous=this.head;
+                --this.length;
+                return true;
+              }
             }
             currentNode=currentNode.next;
+            ++idx;
         }
         // Default is
             return false;        
@@ -84,7 +103,7 @@ class DoublyLinkedList{
 
 class MyHashSet{
     constructor(){
-        this.size=10;
+        this.size=20;
         this.hTable = new Array(this.size);        
     }
     hash(key){
@@ -101,7 +120,7 @@ class MyHashSet{
             if(linkedList.contains(key)){
                 return false;
             }
-        linkedList.add(key);
+        return linkedList.add(key);
         //console.log(this.hTable);
         
     }
@@ -115,6 +134,7 @@ class MyHashSet{
         let linkedList = this.hTable[idx];            
         return linkedList.remove(key);
     }
+
     contains(key){
         let idx=this.hash(key);
         // is there linked list at that index         
@@ -129,39 +149,228 @@ class MyHashSet{
 }
 
 let hset = new MyHashSet();
-hset.add(58);
-hset.add(14);
-hset.remove(91);
-hset.add(6);
-hset.add(66);
-hset.add(51);
-hset.add(16);
-hset.add(40);
-hset.add(52);
-hset.add(48);
-hset.add(42);
-hset.add(85);
-hset.add(36);
-hset.remove(0);
+console.log(hset.add(448));
+console.log(hset.remove(152));
+console.log(hset.add(38));
+console.log(hset.add(105));
+console.log(hset.add(217));
+console.log(hset.contains(105));
+console.log(hset.add(241));
+console.log(hset.remove(571));
+console.log(hset.add(868));
+console.log(hset.add(199));
+console.log(hset.remove(477));
+console.log(hset.add(987));
+console.log(hset.add(272));
+console.log(hset.add(106));
+console.log(hset.add(854));
+console.log(hset.contains(199));
+console.log(hset.add(950));
+console.log(hset.remove(312));
+console.log(hset.remove(505));
+console.log(hset.contains(272));
+console.log(hset.add(108));
+console.log(hset.contains(749));
+console.log(hset.add(826));
+console.log(hset.contains(108));
+console.log(hset.add(258));
+console.log(hset.add(452));
+console.log(hset.remove(642));
+console.log(hset.add(168));
+console.log(hset.add(740));
+console.log(hset.add(915));
+console.log(hset.add(800));
+console.log(hset.add(623));
+console.log(hset.remove(844));
+console.log(hset.remove(371));
+console.log(hset.add(937));
+console.log(hset.remove(790));
+console.log(hset.add(809));
+console.log(hset.contains(604));
+console.log(hset.add(193));
+console.log(hset.add(338));
+console.log(hset.add(385));
+console.log(hset.remove(917));
+console.log(hset.remove(150));
+console.log(hset.add(350));
+console.log(hset.add(729));
+console.log(hset.remove(335));
+console.log(hset.add(553));
+console.log(hset.remove(467));
+console.log(hset.add(967));
+console.log(hset.add(93));
+console.log(hset.remove(283));
+console.log(hset.contains(848));
+console.log(hset.add(199));
+console.log(hset.add(410));
+console.log(hset.add(133));
+console.log(hset.add(161));
+console.log(hset.add(40));
+console.log(hset.contains(553));
+console.log(hset.add(275));
+console.log(hset.contains(410));
+console.log(hset.remove(661));
+console.log(hset.add(899));
+console.log(hset.add(387));
+console.log(hset.add(948));
+console.log(hset.add(758));
+console.log(hset.add(732));
+console.log(hset.add(419));
+console.log(hset.add(735));
+console.log(hset.contains(133));
+console.log(hset.add(630));
+console.log(hset.remove(178));
+console.log(hset.add(13));
+console.log(hset.add(744));
+console.log(hset.contains(168));
+console.log(hset.add(568));
+console.log(hset.add(14));
+console.log(hset.contains(105));
+console.log(hset.add(702));
+console.log(hset.remove(616));
+console.log(hset.add(304));
+console.log(hset.add(678));
+console.log(hset.add(240));
+console.log(hset.contains(677));
+console.log(hset.add(923));
+console.log(hset.add(734));
+console.log(hset.remove(294));
+console.log(hset.add(511));
+console.log(hset.remove(824));
+console.log(hset.add(347));
+console.log(hset.add(586));
+console.log(hset.add(129));
+console.log(hset.remove(609));
+console.log(hset.add(54));
+console.log(hset.add(320));
+console.log(hset.add(717));
+console.log(hset.add(6));
+console.log(hset.contains(106));
+console.log(hset.add(410));
+console.log(hset.add(365));
+console.log(hset.add(48));
+console.log(hset.add(439));
+console.log(hset.remove(135));
+console.log(hset.add(94));
+console.log(hset.add(321));
+console.log(hset.add(665));
+console.log(hset.add(633));
+console.log(hset.add(109));
+console.log(hset.add(882));
+console.log(hset.add(786));
+console.log(hset.contains(38));
+console.log(hset.add(946));
+console.log(hset.contains(426));
+console.log(hset.contains(287));
+console.log(hset.add(371));
+console.log(hset.remove(980));
+console.log(hset.contains(20));
+console.log(hset.add(628));
+console.log(hset.add(925));
+console.log(hset.add(450));
+console.log(hset.remove(785));
+console.log(hset.add(495));
+console.log(hset.remove(87));
+console.log(hset.add(649));
+console.log(hset.contains(379));
+console.log(hset.add(377));
+console.log(hset.remove(984));
+console.log(hset.remove(140));
+console.log(hset.add(120));
+console.log(hset.add(398));
+console.log(hset.add(708));
+console.log(hset.contains(665));
+console.log(hset.add(960));
+console.log(hset.contains(946));
+console.log(hset.add(585));
 console.log(hset.contains(40));
-hset.add(3);
-hset.remove(25);
-hset.add(99);
-hset.remove(66);
-hset.add(60);
-hset.remove(58);
-hset.add(97);
-hset.add(35);
-hset.add(65);
-hset.add(41);
-hset.add(10);
-hset.add(37);
-hset.add(65);
-hset.remove(37);
-hset.add(28);
-hset.add(30);
-hset.add(63);
-hset.add(76);
-hset.remove(90);
+console.log(hset.add(524));
+console.log(hset.add(535));
+console.log(hset.add(957));
+console.log(hset.contains(94));
+console.log(hset.add(214));
+console.log(hset.add(552));
+console.log(hset.add(481));
+console.log(hset.contains(436));
+console.log(hset.add(395));
+console.log(hset.add(526));
+console.log(hset.add(538));
+console.log(hset.contains(923));
+console.log(hset.remove(411));
+console.log(hset.add(563));
+console.log(hset.contains(371));
+console.log(hset.contains(481));
+console.log(hset.add(590));
+console.log(hset.add(500));
+console.log(hset.add(550));
+console.log(hset.add(838));
+console.log(hset.contains(639));
+console.log(hset.add(198));
+console.log(hset.add(63));
+console.log(hset.remove(742));
+console.log(hset.add(579));
+console.log(hset.add(587));
+console.log(hset.remove(55));
+console.log(hset.add(208));
+console.log(hset.contains(347));
+console.log(hset.add(190));
+console.log(hset.add(297));
+console.log(hset.add(357));
+console.log(hset.remove(421));
+console.log(hset.remove(333));
+console.log(hset.add(955));
+console.log(hset.add(504));
+console.log(hset.add(275));
+console.log(hset.add(134));
+console.log(hset.remove(13));
+console.log(hset.remove(414));
+console.log(hset.add(44));
+console.log(hset.remove(610));
+console.log(hset.add(522));
+console.log(hset.add(18));
+console.log(hset.contains(106));
+console.log(hset.add(78));
+console.log(hset.remove(641));
+console.log(hset.add(391));
+console.log(hset.add(850));
+console.log(hset.add(167));
+console.log(hset.add(275));
+console.log(hset.contains(448));
+console.log(hset.remove(2));
+console.log(hset.contains(854));
+console.log(hset.add(15));
+console.log(hset.add(577));
+console.log(hset.contains(133));
+console.log(hset.add(366));
+console.log(hset.add(484));
+console.log(hset.add(125));
+console.log(hset.add(507));
+console.log(hset.add(775));
+console.log(hset.add(38));
+console.log(hset.add(670));
+console.log(hset.remove(179));
+console.log(hset.add(412));
+console.log(hset.remove(866));
+console.log(hset.add(514));
+console.log(hset.add(355));
+console.log(hset.remove(537));
+console.log(hset.remove(143));
+console.log(hset.remove(843));
+console.log(hset.add(907));
+console.log(hset.remove(35));
+console.log(hset.add(579));
+console.log(hset.contains(412));
+console.log(hset.add(118));
+console.log(hset.contains(850));
+console.log(hset.contains(814));
+console.log(hset.add(174));
+console.log(hset.add(828));
+console.log(hset.add(591));
+console.log(hset.add(520));
+console.log(hset.add(678));
+console.log(hset.add(50));
+console.log(hset.add(187));
+console.log(hset.remove(717));
 console.log(hset);
+console.log(hset.contains(717));
 
